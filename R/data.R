5bd76a590d350d40cb443af55660d328834f0ed5
Eduard Bukin <edwardbukin@gmail.com>
2019-05-08 21:55:03 +0200

    Adding data and its documentation

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Sample dataset on automobiles borrowed from STATA.
+#'
+#' @format A data frame with 74 rows and 16 variables
+#'
+#' @source \url{http://www.stata-press.com/data/r15/auto.dta}
+"p_0_exmple"
+
+#' Sub-sample of the automobiles data from STATA.
+#'
+#' @format A data frame with 7 rows and 16 variables
+#'
+#' @source \url{http://www.stata-press.com/data/r15/auto.dta}
+"p_1_exmple"
+
+
+#' Bootstrap permutation vectors sample dataframe
+#'
+#' @format A data frame with 73 rows and 5 columns. Each column represents one
+#'       bootstrap iteration.
+"sample_bootstrap"
